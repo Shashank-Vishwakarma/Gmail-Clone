8be92cd75beba099e0b3463cfe8a36db4a417fc8
Shashank <shashank78680@gmail.com>
2024-07-26 12:11:28 +0530

    added back button in mail info

--- src/components/MailInfo.jsx
+++ src/components/MailInfo.jsx
@@ -1,6 +1,7 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { FaUserCircle } from "react-icons/fa";
-import { useParams } from 'react-router-dom';
+import { IoIosArrowRoundBack } from "react-icons/io";
+import { useNavigate, useParams } from 'react-router-dom';
 
 const MailInfo = () => {
     // const [email, setEmail] = useState({});
@@ -16,8 +17,15 @@ const MailInfo = () => {
         time: "21 May"
     }
 
+    const navigateTo = useNavigate();
+
+    const moveToInbox = ()=>{
+        navigateTo('/');
+    }
+
     return (
         <div className=' w-full my-8 p-4 flex flex-col gap-4 border border-gray-400 bg-slate-50 rounded-md overflow-y-scroll'>
+            <IoIosArrowRoundBack size={25} onClick={moveToInbox} style={{ cursor: 'pointer' }}/>
             <div className='flex flex-col gap-4 bg-slate-100 p-2'>
                 <div className='flex flex-col gap-3'>
                     <span className='mx-10 font-bold text-2xl'>{email?.subject}</span>
